689820efa7624a98bb06005fc0e2a1420c7a6c46
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-23 18:36:27 +0530

    just testing the things now

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 const http = require('http')
 const app = require('./app')
+const port = process.env.PORT || 300 
 
+const server = http.createServer(app)
+
+server.listen(port,()=>{
+ console.log("server is up on port 3000")
+})
