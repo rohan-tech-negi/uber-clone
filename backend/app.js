632a870832db26c97a57784aff2c2dd6fb1c8cba
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-23 18:44:59 +0530

    setup a db connection file

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const connectToDb = require('./db/db');
 const app = express();
 
 app.use(cors())
+connectToDb()
 
 app.get("/", (req,res)=>{
     res.send('hello world')
--- backend/db/db.js
+++ backend/db/db.js
@@ -1,2 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+const connectToDb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      process.env.DB_CONNECT
+    );
+
+    console.log(
+      `MongoDB Connected: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+
+    // Exit process if DB connection fails
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDb;
